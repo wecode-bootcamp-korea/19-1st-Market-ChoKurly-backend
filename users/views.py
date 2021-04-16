6a66e42c16a9a7b59e3bc79a558d5a665bb84a66
ahnjeonghyun <tgrf3090@gmail.com>
2021-04-16 16:49:20 +0900

    [19기 안정현] Refactor: 회원가입 멘토리뷰 반영 conflict 수정

--- users/views.py
+++ users/views.py
@@ -1,14 +1,40 @@
-import json
-<<<<<<< HEAD
 import bcrypt
 import jwt
 import my_settings
+import json
 
-from django.db          import transaction
-from django.http        import JsonResponse
-from django.views       import View
+from django.http          import JsonResponse
+from django.views         import View
+from django.db            import transaction
+from django.views         import View
+from django.http          import JsonResponse
+from django.db.models     import Q
 
-from .models            import User, Address
+from users.models         import User, Address, Review, UserLike, Comment
+
+class FindIdView(View):
+    def post(self, request):
+
+        try:
+            data  = json.loads(request.body)
+            name  = data['name']
+            email = data['email']
+
+            if not my_settings.EMAIL_CHECK.match(email):
+                return JsonResponse({'MESSAGE':'EMAIL_TYPE_ERROR'}, status=400)
+
+            if not User.objects.filter(Q(name=name) & Q(email=email)).exists():
+                return JsonResponse({'MESSAGE':'INVALID_EMAIL_OR_INVALID_NAME'}, status=400)
+
+            user = User.objects.get(email=email)
+
+            return JsonResponse({'MESSAGE':user.identification}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'MESSAGE':'JSON_Decode_Error'}, status=400)
+
 
 class UserView(View):
     def post(self, request):
@@ -79,45 +105,11 @@ class UserView(View):
 
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
-=======
-import my_settings
-
-from django.views         import View
-from django.http          import JsonResponse
-from django.db.models     import Q
-
-from users.models         import User, Address, Review, UserLike, Comment
-
-class FindIdView(View):
-    def post(self, request):
-
-        try:
-            data  = json.loads(request.body)
-            name  = data['name']
-            email = data['email']
-
-            if not my_settings.EMAIL_CHECK.match(email):
-                return JsonResponse({'MESSAGE':'EMAIL_TYPE_ERROR'}, status=400)
-
-            if not User.objects.filter(Q(name=name) & Q(email=email)).exists():
-                return JsonResponse({'MESSAGE':'INVALID_EMAIL_OR_INVALID_NAME'}, status=400)
-
-            user = User.objects.get(email=email)
-
-            return JsonResponse({'MESSAGE':user.identification}, status=200)
-
-        except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({'MESSAGE':'JSON_Decode_Error'}, status=400)
 
 
 
 
 
-
-
->>>>>>> main
 
 
 
