07a14a093753772da32abcfc15ba4d4da5d64438
ahnjeonghyun <tgrf3090@gmail.com>
2021-04-16 19:26:14 +0900

    add: users login 기능 구현

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import FindIdView,UserView
+from users.views import FindIdView, LoginView, UserView
 
 urlpatterns = [
     path('/findid', FindIdView.as_view()),
     path('/signup', UserView.as_view()),
+    path('/login', LoginView.as_view())
 ]
