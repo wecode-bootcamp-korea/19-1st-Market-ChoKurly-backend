5ee82d6bc10c1023b5626845877008f4ca89ce49
seungchan baek <poketsc@gmail.com>
2021-04-19 16:37:49 +0900

    [19기 백승찬] Refactor:전체카테고리-서브카테고리 리스트 컴프리헨션으로 수정

--- products/views.py
+++ products/views.py
@@ -6,23 +6,15 @@ from products.models import Product, Category, SubCategory, Allergy, ProductInfo
 class CategoryView(View):
     def get(self, request):
         categories = Category.objects.all()
-        results = []
-        for category in categories:
-            sub_categories = SubCategory.objects.filter(category_id=category.id)
-            sub_category_lists = []
-            for sub_category in sub_categories:
-                sub_category_lists.append(
-                    {
-                    'id':sub_category.id,
-                    'name':sub_category.name
-                    }
-                )
-            results.append(
-                {
-                    'id' : category.id,
-                    'category': category.name,
-                    'sub_category': sub_category_lists
 
-                }
-            )
-        return JsonResponse({'results':results}, status=200)
+        results = [
+            {
+                'id'          : category.id,
+                'category'    : category.name,
+                'sub_category': [{
+                    'id'  : subcategory.id,
+                    'name': subcategory.name
+                } for subcategory in category.subcategory_set.all()]
+            } for category in categories]
+
+        return JsonResponse({'results': results}, status=200)
