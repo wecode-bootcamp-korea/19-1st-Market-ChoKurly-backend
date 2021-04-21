164162081610277480d0c187abd1ac691cb35d6f
seungchan baek <poketsc@gmail.com>
2021-04-21 16:48:51 +0900

    [19기 백승찬] Conflict 수정

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-import json
+import random
 
 from django.views    import View
 from django.http     import JsonResponse
@@ -7,8 +7,7 @@ from products.models import (
     Product, Category,
     SubCategory, Allergy,
     ProductInformation,
-    AllergyProduct, Image,
-    DiscountRate)
+    AllergyProduct, DiscountRate)
 
 class CategoryView(View):
     def get(self, request):
@@ -58,26 +57,64 @@ class ProductListView(View):
 
         return JsonResponse({'RESULTS':results}, status=200)
 
-class SearchView(View):
-    def get(self,request):
-        search_content = request.GET.get('search_content', None)
+class ProductDetailView(View):
+    def get(self, request, product_id=None):
+        if not product_id:
+            return JsonResponse({'message':'ENTER product_id'}, status=400)
 
-        if not search_content:
-            return JsonResponse({'MESSAGE':'INVALID_CONTENT'}, status=400)
+        product                 = Product.objects.filter(id=product_id).first()
 
-        products = Product.objects.filter(name__icontains=search_content)
+        if not product:
+            return JsonResponse({'message':'UNKNOWN_PRODUCT'}, status=400)
+        
+        product_info            = product.productinformation
 
-        results = [
-            {
-                "id": product.id,
-                "name": product.name,
-                "original_price": int(product.price),
-                "discount_rate": float(product.discount_rate.discount_rate) if product.discount_rate else None,
-                "discounted_price": int(product.price - (product.price * product.discount_rate.discount_rate)) if product.discount_rate else None,
-                "thumbnail_image": product.thumbnail_image,
-                "sticker": product.sticker.name if product.sticker else None,
-                "comment":product.productinformation.comment
+        allergy_list            = [{
+            'id'        : '{}'.format(index+1),
+            'allergy_id': product_info.allergy.all()[index].id,
+            'allergy'   : product_info.allergy.all()[index].name, 
+        } for index in range(len(product_info.allergy.all()))]
 
-            } for product in products]
+        picked_related_products = self.pick_related_product([
+            related_product for related_product in Product.objects.all()
+        ])
+        
+        related_product_list    = self.make_related_product_list(picked_related_products) 
+#
+        result                  = [{
+            'id'                : "1",
+            'product_id'        : product.id,
+            'discount_rate'     : float(product.discount_rate.discount_rate), # 할인율
+            'name'              : product.name,                               # 상품명
+            'comment'           : product_info.comment,                       # 상품 코멘트
+            'price'             : int(product.price),                         # 상품가격
+            'sale_unit'         : product_info.sale_unit,                     # 판매단위
+            'weight_g'          : float(product_info.weight_g),               # 중량/용량
+            'delivery_type'     : product_info.delivery_type,                 # 배송구분
+            'packing_type'      : product_info.packing_type,                  # 포장타입
+            'allergy'           : allergy_list,                               # 알레르기 정보
+            'instruction'       : product_info.instruction,                   # 안내사항
+            'review'            : None,                                       # 상품 리뷰
+            'thumbnail_image'   : product.thumbnail_image,                    # 상품 섬네일 이미지
+            'description_image' : product.description_image,                  # 상품 description_image
+            'size_image'        : product.size_image,                         # 상품 size_image
+            'related_products'  : related_product_list,                       # 관련 상품
+            }]
 
-        return JsonResponse({'RESULTS':results}, status=200)
+        return JsonResponse({'result':result}, status=200)
+
+    def pick_related_product(self, info):
+        random.shuffle(info)
+        result = [info[index] for index in range(10)]
+        return result
+
+    def make_related_product_list(self, picked_related_products):
+        result           = [{
+            'id'         : '{}'.format(index+1),
+            'product_id' : picked_related_products[index].id,
+            'name'       : picked_related_products[index].name,
+            'price'      : int(picked_related_products[index].price),
+            'rel_img'    : picked_related_products[index].thumbnail_image,
+        } for index in range(len(picked_related_products))]
+        return result
+
