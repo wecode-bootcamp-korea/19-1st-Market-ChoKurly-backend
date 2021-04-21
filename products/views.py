bdbcda4aa208c990a1531c6d3b82263d5322702a
seungchan baek <poketsc@gmail.com>
2021-04-21 10:40:32 +0900

    [19기 백승찬] Refactor:PrductListView 페이지네이션 기능 추가

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ class CategoryView(View):
     def get(self, request):
         categories = Category.objects.all()
 
-        results = [
+        RESULTS = [
             {
                 'id'          : category.id,
                 'category'    : category.name,
@@ -22,28 +22,31 @@ class CategoryView(View):
                 } for subcategory in category.subcategory_set.all()]
             } for category in categories]
 
-        return JsonResponse({'results': results}, status=200)
+        return JsonResponse({'RESULTS': RESULTS}, status=200)
 
 class ProductListView(View):
     def get(self,request):
         category_id     = request.GET.get('category_id', None)
         sub_category_id = request.GET.get('sub_category_id', None)
         order_by_type   = request.GET.get('order_by_type', None)
-        product_count   = int(request.GET.get('product_count', 9))
+        page            = int(request.GET.get('page', 1))
+        limit           = int(request.GET.get('limit', 8))
+        start           = (page - 1) * limit
+        end             = page * limit
 
         if not category_id and not sub_category_id:
-            products = Product.objects.order_by(order_by_type)[:product_count]
+            products = Product.objects.order_by(order_by_type)[start:end]
         if category_id:
-            products = Product.objects.filter(category_id=category_id).order_by(order_by_type)[:product_count]
+            products = Product.objects.filter(category_id=category_id).order_by(order_by_type)[start:end]
         if sub_category_id:
-            products = Product.objects.filter(sub_category_id=sub_category_id).order_by(order_by_type)[:product_count]
+            products = Product.objects.filter(sub_category_id=sub_category_id).order_by(order_by_type)[start:end]
 
-        results = [{
+        RESULTS = [{
 
             "id": product.id,
             "name": product.name,
             "original_price": int(product.price),
-            "discount_rate": product.discount_rate.discount_rate if product.discount_rate else None,
+            "discount_rate": float(product.discount_rate.discount_rate) if product.discount_rate else None,
             "discounted_price": int(product.price - (product.price * product.discount_rate.discount_rate)) if product.discount_rate else None,
             "thumbnail_image": product.thumbnail_image,
             "sticker": product.sticker.name if product.sticker else None,
@@ -51,4 +54,4 @@ class ProductListView(View):
 
         } for product in products]
 
-        return JsonResponse({'results':results}, status=200)
+        return JsonResponse({'RESULTS':RESULTS}, status=200)
